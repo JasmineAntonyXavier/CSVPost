b52bfa38cdb0a7440707d5a2c86d2b7478aacb52
Jasmine Antony Xavier <jasjesuraj@gmail.com>
2018-03-14 23:14:10 -0400

    Updating to new endpoint and csv file to be read and posted

--- requestTest.js
+++ requestTest.js
@@ -1,15 +1,14 @@
 var request = require('request');
 var LineByLineReader = require('line-by-line'),
 
-lr = new LineByLineReader('stateandcapital.csv');
+lr = new LineByLineReader('StateCapitalLatLong.csv');
 
 var count = 0;
 lr.on('line', function (line) {
 	count ++;
 	if (count != 1){
 		var res = line.split(",");
-		console.log(res[0]);
-		var jsonDataObj = { state_name: res[0], capital_name: res[1] };
+		var jsonDataObj = { state_name: res[0], capital_name: res[1], latitude: res[2], longitude: res[3] };
 		request.post({
 			url: 'http://localhost:1000/states/',
 			body: jsonDataObj,
